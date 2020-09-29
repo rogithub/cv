7270016087e30c20398c1a6696ea7c470806d6c0
rodrigo debian <correo.rodrigo@gmail.com>
2020-09-29 13:21:38 -0500

    moving button out of content

--- src/views/index.vue
+++ src/views/index.vue
@@ -15,7 +15,7 @@ div.cv {
 
 <template>
   <div class="container">
-    <div class="cv rounded mb-4">
+    <div class="cv rounded mb-2">
       <h1 class="display-4">Rodrigo Jiménez</h1>
       <p class="lead">Software developer</p>
 
@@ -219,29 +219,29 @@ div.cv {
           Bachelor's degree, Telecommunications Engineering
         </h5>
       </div>
+    </div>
 
-      <div class="text-right">
-        <a class="btn btn-primary" href="#" role="button">
-          Top
-          <svg
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            class="bi bi-arrow-up-square"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"
-            />
-          </svg>
-        </a>
-      </div>
+    <div class="text-right mb-2">
+      <a class="btn btn-primary" href="#" role="button">
+        Top
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          class="bi bi-arrow-up-square"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"
+          />
+        </svg>
+      </a>
     </div>
   </div>
 </template>
